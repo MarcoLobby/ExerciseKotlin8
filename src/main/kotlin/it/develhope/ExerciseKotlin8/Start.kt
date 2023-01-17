3f87bdfd2620a1b495602322185ac3496eef7af4
Marco Lo Bello <xSalamander@outlook.it>
2023-01-17 12:25:09 +0100

    Initial Commit

--- src/main/kotlin/it/develhope/ExerciseKotlin8/Start.kt
+++ src/main/kotlin/it/develhope/ExerciseKotlin8/Start.kt
@@ -5,11 +5,16 @@ Then create a list that that has at least 10 members, filter them and then print
 
 fun main (){
 
-    val numbers: List <Int> = listOf(1,2,30,400,5000)
-    numbers.filterSmaller().forEach{ println("$it") }
+    val numbers: List <Int> = listOf(1,2,30,400,500,4999,5000,9000,34567,10000)
+    numbers.filterSmaller().print()
 }
 
 fun List<Int>.filterSmaller(): List <Int>{
-    return this.filter { it -> it < 500 }
+    return this.filter { it < 500 }
+}
+
+fun List<Int>.print(){
+    this.forEach { println(it) }
+
 }
 
